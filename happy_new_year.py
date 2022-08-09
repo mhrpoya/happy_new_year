7b6ff81fbc0c4b22aa08263dcaee40dca7230cd0
mhrpoya <mehrpoyakhosravi49@gmail.com>
2022-08-09 13:06:44 +0430

    new3

--- happy_new_year.py
+++ happy_new_year.py
@@ -1,19 +1,22 @@
+from cgi import print_environ
 from time import sleep
 
 from click import echo
 
 item = 1
 wid = 50
+wid_2 = 80
+party = "\U0001f973"
 
 for _ in range(15):
-    echo(("+" * item).center(wid))
+    echo((party * item).center(wid))
     item += 2
     sleep(0.5)
 
-echo("| HAPPY NEW YEAR |".center(wid))
-echo("\ 2022 /".center(wid))
+echo("/ HAPPY NEW YEAR \\".center(wid_2))
+echo("\ 2022 /".center(wid_2))
 
 for _ in range(16, 0, -1):
-    echo(("+" * item).center(wid))
+    echo((party * item).center(wid))
     item -= 2
     sleep(0.5)
